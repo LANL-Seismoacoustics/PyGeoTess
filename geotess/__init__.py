009d118efc57689476634ec34f3f450a13992d88
Jonathan MacCarthy <jkmacc@lanl.gov>
2015-11-10 16:49:08 -0700

    Add things into main geotess namespace

--- geotess/__init__.py
+++ geotess/__init__.py
@@ -1,4 +1,8 @@
-import os
-import sys
+#import os
+#import sys
+#
+#sys.path.append(os.path.dirname(__file__) + os.path.sep + 'lib' + os.path.sep + 'geotess.jar')
 
-sys.path.append(os.path.dirname(__file__) + os.path.sep + 'lib' + os.path.sep + 'geotess.jar')
+from geotess.utils import Layer, Attribute
+from geotess.grid import Grid
+from geotess.model import Model
