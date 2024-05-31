d5938a5c0cbf215df19335f920732470bf49ad20
Jonathan MacCarthy <jkmacc@lanl.gov>
2024-05-31 11:34:03 -0600

    Go back to function-based tests for consistency

--- tests/test_GeoTessMetaData.py
+++ tests/test_GeoTessMetaData.py
@@ -2,15 +2,8 @@
 """
 import geotess.libgeotess as libgt
 
-class TestGeoTessMetaData:
-    def test_init(self):
-        # Test empty/nullary constructor
-        # Just don't want it to crash the interpreter.
-        md = libgt.GeoTessMetaData()
-        del md
-
-# def test_GeoTessMetaData__init__():
-#     # Test empty/nullary constructor
-#     # Just don't want it to crash the interpreter.
-#     md = libgt.GeoTessMetaData()
-#     del md
+def test_init():
+    # Test empty/nullary constructor
+    # Just don't want it to crash the interpreter.
+    md = libgt.GeoTessMetaData()
+    del md
