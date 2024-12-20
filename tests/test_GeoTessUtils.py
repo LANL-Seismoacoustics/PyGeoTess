2bf46c62c980a0cfc00571920ca5da106de4833d
Jonathan MacCarthy <jkmacc@lanl.gov>
2024-12-20 16:51:37 -0700

    Consistent test syntax

--- tests/test_GeoTessUtils.py
+++ tests/test_GeoTessUtils.py
@@ -1,7 +1,7 @@
 """ Tests for libgeotess.GeoTessUtils
 """
 import numpy as np
-import geotess.libgeotess as lib
+import geotess.libgeotess as libgt
 import pytest
 
 LAT = 30.0
@@ -10,25 +10,25 @@ R = 6372.824420335703
 VEC = np.array([0.61339643, 0.61339643, 0.4974833])
 
 def test_init():
-    utils = lib.GeoTessUtils()
+    utils = libgt.GeoTessUtils()
     del utils
 
 def test_getLatDegrees():
     expected = LAT
-    observed = lib.GeoTessUtils.getLatDegrees(VEC)
+    observed = libgt.GeoTessUtils.getLatDegrees(VEC)
     np.testing.assert_approx_equal(expected, observed)
 
 def test_getLonDegrees():
     expected = LON
-    observed = lib.GeoTessUtils.getLonDegrees(VEC)
+    observed = libgt.GeoTessUtils.getLonDegrees(VEC)
     np.testing.assert_approx_equal(expected, observed)
 
 def test_getVectorDegrees():
     expected = VEC
-    observed = lib.GeoTessUtils.getVectorDegrees(LAT, LON)
+    observed = libgt.GeoTessUtils.getVectorDegrees(LAT, LON)
     np.testing.assert_array_almost_equal(expected, observed)
 
 def test_getEarthRadius():
     expected = R
-    observed = lib.GeoTessUtils.getEarthRadius(VEC)
+    observed = libgt.GeoTessUtils.getEarthRadius(VEC)
     assert observed == pytest.approx(expected)
